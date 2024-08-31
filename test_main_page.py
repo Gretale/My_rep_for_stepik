68fd269787e6f599cd6a33df7e2bd990a9e3ab87
Grezhshchuk <gretalexan@gmail.com>
2024-08-31 12:46:48 +0500

    cheks_url_login_register_forms

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,17 @@
 from time import sleep
-
-from selenium.webdriver.common.by import By
-from .Pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
+#ages.main_page import (MainPage)
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) #Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    login = LoginPage(browser, link)
     page.open() # открываем страницу
-    sleep(5)
     page.go_to_login_page()   # выполняем метод страницы — переходим на страницу логина
+    login.should_be_login_page()
+
     sleep(5)
 
 def test_guest_should_see_login_link(browser):
@@ -19,3 +21,4 @@ def test_guest_should_see_login_link(browser):
     sleep(5)
     page.should_be_login_link()
     sleep(5)
+
